5f06d7b98ec00ebc667e7c813a0f284883546924
Thiago Diniz da Silveira <thiagods.ti@gmail.com>
2016-10-27 18:52:28 -0200

    Primeiro commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+
+// =============== Base libraries integration ==================
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import VueTranslate from 'vue-translate-plugin';
+
+Vue.use(VueResource);
+Vue.use(VueTranslate);
+
+import store from './store';
+import router from './router';
+
+// ===== Bootstrap components integration (JQuery needed) ======
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-sass');
+
+// ======================= Base Component ======================
+import App from './App';
+
+// ======================== Vue Instance =======================
+/* eslint-disable no-new */
+new Vue({
+  router,
+  store,
+  el: '#app',
+  render: h => h(App),
+});
